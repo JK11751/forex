5f9ab1dc12750ee81d8ec428a8f223700426b0a8
Gimo <kgimase@gmail.com>
2023-01-23 06:47:20 +0300

    loading

--- src/App.js
+++ src/App.js
@@ -1,41 +1,38 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import {
-  ChakraProvider,
   Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import Home from './pages/Home';
+import NavBar from './components/NavBar';
+import Investment from './pages/Investment';
+import Testimonials from './pages/Testimonials';
+import About from './pages/About';
+import Loading  from './components/Loading';
+
 
 function App() {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 6000)
+  }, [])
+
   return (
-    <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
-    </ChakraProvider>
+    <>
+    {loading === false ? (
+      
+    <Box>
+    <NavBar/>
+    <Home/>
+    <About/>
+     <Investment/>
+     <Testimonials/>
+    </Box>
+    ) : (
+        <Loading />
+      )}
+
+    </>
   );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,92 @@
+import { React } from 'react';
+import {
+  Box,
+  Flex,
+  Switch,
+  HStack,
+  Link,
+  IconButton,
+  Button,
+  useColorMode,
+  ButtonGroup,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+} from '@chakra-ui/react';
+import { HamburgerIcon, CloseIcon} from '@chakra-ui/icons';
+
+const Links = ['Home', 'About','Team', 'Contact'];
+
+const NavLink = ({ children }) => (
+  <Link
+    px={2}
+    py={1}
+    rounded={'md'}
+    _hover={{
+      textDecoration: 'none',
+      bg: useColorModeValue('gray.200', 'gray.700'),
+    }}
+    href={'#'}
+  >
+    {children}
+  </Link>
+);
+
+const NavBar = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { colorMode, toggleColorMode } = useColorMode();
+  const isDark = colorMode === 'dark';
+
+  return (
+    <>
+      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+          <IconButton
+            size={'md'}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={'Open Menu'}
+            display={{ md: 'none' }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={8} alignItems={'center'}>
+            <Box>Logo</Box>
+            
+            <HStack
+            pl={200}
+              as={'nav'}
+              spacing={4}
+              display={{ base: 'none', md: 'flex' }}
+            >
+              {Links.map(link => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </HStack>
+          </HStack>
+          <Flex alignItems={'center'}>
+            <ButtonGroup gap="2">
+              <Button colorScheme="teal">Sign Up</Button>
+              <Button colorScheme="teal">Log in</Button>
+            </ButtonGroup>
+            <Switch
+             pl={4}
+              color="green"
+              isChecked={isDark}
+              onChange={toggleColorMode}
+            />
+          </Flex>
+        </Flex>
+
+        {isOpen ? (
+          <Box pb={4} display={{ md: 'none' }}>
+            <Stack as={'nav'} spacing={4}>
+              {Links.map(link => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
+    </>
+  );
+};
+export default NavBar;
